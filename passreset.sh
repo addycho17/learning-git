876d656ac7670cb61300fa81034e5124e50922bd
Adeline Chofong <ngholapehadeline@gmail.com>
2022-02-23 17:39:44 +0000

    two files added

---
+++ passreset.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+# This script is for password reset
+username=name
+password=1234
+echo "please enter your $name"
+echo "please enter your password"
+if [ password=1234 ]
+	echo "login sucessful"
+else
+	echo "invalid password"
+	echo "click on the link to reset password"
+fi
---
+++ user_log.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+echo welcome to navy federal credit union
+echo "please enter your name to proceed"
+read name
+echo $name please enter your pin
+read -s pin
+echo "thanks $name for verifying your information"
